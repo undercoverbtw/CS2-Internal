390c6da531560cc241f878a5a7954feef2d873c3
only back for cs2 <39854659+undercoverbtw@users.noreply.github.com>
2023-11-15 17:02:59 +0100

    [0.1] [entry-point]
    Proper dllmain & cheat-thread. Everything is easy to understand and added good explanations if needed.

--- cs2-internal/source/main.cpp
+++ cs2-internal/source/main.cpp
@@ -6,7 +6,7 @@ unsigned long __stdcall init(void* pm)
 #pragma region debuglogs
 #ifdef _DEBUG
 
-    // debug console with logs will come in next update
+	// debug console with logs will come in next update
 	// debug logs are very importmant and really useful for debugging crashes and where we crash without having to attach a debugger
 
 #endif // _DEBUG
@@ -24,14 +24,14 @@ unsigned long __stdcall init(void* pm)
 
 		// unloading cheat when pressing END key
 		// if we have not clicked the thread will sleep until unload button preseed
-		
+
 		while (!(GetAsyncKeyState(VK_END))) // figure this out :)
 			std::this_thread::sleep_for(std::chrono::milliseconds(100)); // sleep thread
 
 	} while (false);
 #pragma endregion initialization
-	
-	
+
+
 #pragma region unload
 	// this code will be executed when unload button is pressed
 
@@ -47,7 +47,7 @@ unsigned long __stdcall init(void* pm)
 	// close file (log file)
 	// close debug console
 #endif // debug
-	
+
 	// lastly we need to exit our thread that we opened in DLLmain
 	FreeLibraryAndExitThread(static_cast<HMODULE>(pm), EXIT_SUCCESS);
 
@@ -60,14 +60,14 @@ BOOL APIENTRY DllMain(const HMODULE module, const DWORD reason, const LPVOID res
 	switch (reason)
 	{
 	case DLL_PROCESS_ATTACH:
-	    {
+	{
 
 		// creating our thread and making init run inside of the thread
 		if (const HANDLE thread = CreateThread(nullptr, 0, init, module, 0, nullptr); thread != nullptr)
-			CloseHandle(thread); 
+			CloseHandle(thread);
 		// dont really need to explain but we close the thread after creating it.
-		
-	    }
+
+	}
 
 	}
 #pragma endregion injection
